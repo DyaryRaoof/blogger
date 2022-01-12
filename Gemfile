08fe7d6c229f0db2732abbfe54c6fd32df16d77b
DyaryRaoof <dyary2020@gmail.com>
2022-01-12 20:22:20 +0300

    Add test gem and relevant test for users and posts controller

--- Gemfile
+++ Gemfile
@@ -48,6 +48,9 @@ gem 'bootsnap', require: false
 # gem "image_processing", "~> 1.2"
 
 group :development, :test do
+  gem 'capybara'
+  gem 'rspec-rails'
+  gem 'rails-controller-testing'
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
 end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "users", type: :request do
+  describe "GET /users" do
+    it "works! returns correct status code" do
+      get users_path
+      expect(response).to have_http_status(200)
+    end
+
+    it "works! renders index view correctly" do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+
+    it "works! reponse body includes <h1>This is a list of users<h1> " do
+      get users_path
+      expect(response.body).to include("<h1>This is a list of users<h1>")
+    end
+  end
+end
+
+
+
+RSpec.describe "users/:id", type: :request do
+  describe "GET /users/:id" do
+    it "works! returns correct status code" do
+      get user_path(1)
+      expect(response).to have_http_status(200)
+    end
+
+    it "works! renders show view correctly" do
+      get user_path(1)
+      expect(response).to render_template(:show)
+    end
+
+
+    it "works! reponse body includes <h1>this is a single user </h1>" do
+      get user_path(1)
+      expect(response.body).to include("<h1>this is a single user </h1>")
+    end
+  end
+end
