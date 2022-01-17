6a60da66911cec68cc7f0b19b97f2f83d28d3156
DyaryRaoof <dyary2020@gmail.com>
2022-01-17 21:02:48 +0300

    Add models and do CRUD

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :author
-  belongs_to :post
+    belongs_to :user, foreign_key: 'author_id'
+    belongs_to :post
+
+    def update_comments_counter_for_post
+        p = Post.find(self.post_id)
+        p.comments_counter += 1
+        p.save
+    end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,10 @@
 class Like < ApplicationRecord
+    belongs_to :user, foreign_key: 'author_id'
+    belongs_to :post
+
+    def update_likes_counter_for_post
+        p = Post.find(self.post_id)
+        p.likes_counter += 1
+        p.save
+    end
 end
