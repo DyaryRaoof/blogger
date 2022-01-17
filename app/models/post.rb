e21767faffc8d14df1d662fae10c284dc9963b33
DyaryRaoof <dyary2020@gmail.com>
2022-01-17 21:05:42 +0300

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,15 @@
 class Post < ApplicationRecord
-    belongs_to :user, foreign_key: 'author_id'
-    has_many :comment
-    has_many :like
+  belongs_to :user, foreign_key: 'author_id'
+  has_many :comment
+  has_many :like
 
-    def update_posts_counter_for_user
-        u = User.find(self.author_id)
-        u.posts_counter += 1
-        u.save
-    end
+  def update_posts_counter_for_user
+    u = User.find(author_id)
+    u.posts_counter += 1
+    u.save
+  end
 
-    def five_most_recent_comments
-        Comment.where(post_id: self.id).order(created_at: :desc).limit(5)
-    end
+  def five_most_recent_comments
+    Comment.where(post_id: id).order(created_at: :desc).limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-    has_many :comment
-    has_many :like
-    has_many :post
+  has_many :comment
+  has_many :like
+  has_many :post
 
-    def three_most_recent_posts
-        Post.where(author_id: self.id).order(created_at: :desc).limit(3)
-    end
+  def three_most_recent_posts
+    Post.where(author_id: id).order(created_at: :desc).limit(3)
+  end
 end
